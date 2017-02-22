2fa4997f4cc78e40826e61361da2c985d8950242
Pat Wehman <patwehman@gmail.com>
2017-02-22 14:53:09 -0500

    class work

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?  
+require_relative 'coin_changer.rb'
+
+get '/' do
+  erb :get_coins
+end
+
+post '/your_input_change' do
+
+  	amount = params[:amount].to_i
+  	change = get_change(amount)
+ 
+	"Your change from the money changer is #{change}."
+
+end
+
