06fe6858a2bba0536a195d72383e080998094f72
Pat Wehman <patwehman@gmail.com>
2017-02-22 15:37:12 -0500

    class work

--- app.rb
+++ app.rb
@@ -11,7 +11,9 @@ post '/your_input_change' do
   	amount = params[:amount].to_i
   	change = get_change(amount)
  
-	"Your change from the money changer is #{change}."
-
+#	"Your change from the money changer is #{change}."
+	
+	erb :get_next_coins, :locals => {:change => change}
 end
 
+ 
